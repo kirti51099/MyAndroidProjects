cd66bb27e8f1fbb583cab0509d4eb7e0dbc94890
kirti51099 <kirtighatkamble52004@gmail.com>
2026-04-28 22:17:20 +0530

    Add files via upload

---
+++ MultiThreading/app/src/main/java/com/example/multithreaddemo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.multithreaddemo;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(v -> {
+            Thread thread = new Thread(() -> {
+                for (int i = 1; i <= 5; i++) {
+                    try {
+                        Thread.sleep(1000); // delay 1 sec
+
+                        int finalI = i;
+                        runOnUiThread(() -> {
+                            textView.setText("Count: " + finalI);
+                        });
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            thread.start();
+        });
+    }
+}
